fa99369677b58521082ce063f9210fc29676830a
Lance Kian F. Flores <lafa.flores.up@phinmaed.com>
2024-11-16 02:52:03 +0800

    done

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
-// index.js
 const express = require("express");
 const app = express();
 const port = 3000;
+const myRoutes = require("./routes/myRoute");
 
+// Middleware to parse JSON request bodies
+app.use(express.json());
+
+// Use the routes defined in myRoutes
+app.use("/api", myRoutes);
+
+// Basic route for testing
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
--- routes/myRoute.js
+++ routes/myRoute.js
@@ -1,4 +1,3 @@
-// routes/myRoutes.js
 const express = require("express");
 const router = express.Router();
 
@@ -8,5 +7,6 @@ const myController = require("../controllers/myController");
 // Define routes and map them to controller functions
 router.get("/", myController.getHome);
 router.get("/about", myController.getAbout);
+router.post("/contact", myController.createContactMessage); // Added the contact route
 
 module.exports = router;
