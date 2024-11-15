8a97d60d3af7bcf0fa72835d8a24ebc84d895dfe
Lance Kian F. Flores <lafa.flores.up@phinmaed.com>
2024-11-16 03:02:05 +0800

    changes

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require("express");
+const cors = require("cors"); // Import cors package
 const app = express();
 const port = 3000;
 const myRoutes = require("./routes/myRoute");
+
+// Enable CORS for all routes
+app.use(cors());
 
 // Middleware to parse JSON request bodies
 app.use(express.json());
@@ -12,6 +16,11 @@ app.use("/api", myRoutes);
 // Basic route for testing
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+// Catch-all for undefined routes
+app.all("*", (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server`, 404));
 });
 
 app.listen(port, () => {
